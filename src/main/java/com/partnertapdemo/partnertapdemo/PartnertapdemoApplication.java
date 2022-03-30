43b22313021516d9ac514148eb8e6ce940135075
dineshtripathi30 <dinesh.tripathi30@gmail.com>
2022-03-30 17:43:39 +0530

    first commit

--- src/main/java/com/partnertapdemo/partnertapdemo/PartnertapdemoApplication.java
+++ src/main/java/com/partnertapdemo/partnertapdemo/PartnertapdemoApplication.java
@@ -1,7 +1,10 @@
 package com.partnertapdemo.partnertapdemo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PartnertapdemoApplication {
@@ -9,5 +12,8 @@ public class PartnertapdemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PartnertapdemoApplication.class, args);
 	}
-
+	@Bean
+	public HttpTraceRepository htttpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
+	}
 }
